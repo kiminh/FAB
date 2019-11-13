81056dc63774bcb942a3129a1bb81cf62f23e637
JiaXingBingGan <729869585@qq.com>
2019-11-13 10:19:16 +0800

    修改文件生成

--- src/data_type.py
+++ src/data_type.py
@@ -1,6 +1,6 @@
 config = {
     'data_path': '../../data/',
     'campaign_id': '1458/',
-    'type': 'sample',
+    'type': 'sample', # No sample - data; Sample - sample
     'is_gpu': 1, # gpu - 1; cpu - 0
 }
