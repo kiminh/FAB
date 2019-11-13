2415e11bb6b191663f6b05c504d80f8518047347
JiaXingBingGan <729869585@qq.com>
2019-11-13 09:58:12 +0800

    修改DRLB/reward_net_gpu.py

--- src/DRLB/reward_net_gpu.py
+++ src/DRLB/reward_net_gpu.py
@@ -69,7 +69,7 @@ class RewardNet:
         self.model_reward, self.real_reward = Net(self.feature_numbers, self.reward_numbers).cuda(), Net(self.feature_numbers, self.reward_numbers).cuda()
 
         # 优化器
-        self.optimizer = torch.optim.RMSprop(self.eval_net.parameters(), lr=self.lr, alpha=0.95)
+        self.optimizer = torch.optim.RMSprop(self.model_reward.parameters(), lr=self.lr, alpha=0.95)
         # 损失函数为，均方损失函数
         self.loss_func = nn.MSELoss().cuda()
 
